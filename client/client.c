11176988b4b1da54768cfd80d76ff661c9ec58a3
Sebastian <sebastian0krysiuk@gmail.com>
2024-07-28 12:13:45 -0400

    Send hash to client

--- client/client.c
+++ client/client.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <unistd.h> // for close()
-#include <arpa/inet.h> // for init_addr
+#include <unistd.h> 
+#include <arpa/inet.h>
 #include <string.h>
 
 #define BUFFER_SIZE 1024
 
+unsigned char* binaryToHex(unsigned char *buffer, size_t size){
+    unsigned char *hexString;
+    for(size_t i=0; i<size; ++i){
+        sprintf(&hexString[i * 2], "%02x", buffer[i]);
+    }
+    // printf("The hex is: %s", hexString);
+    return hexString;
+}
+
 int main(int argc, char *argv[])
 {
-    char buffer[BUFFER_SIZE];
-    char buffer2[BUFFER_SIZE];
+    char *buffer = (char*) calloc(BUFFER_SIZE, sizeof(char));
     int sockid;
     int server_port = 8888;
     char *server_ip = "127.0.0.1";
+    char *msg = argv[1]; // le nom de fichier
 
     sockid = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -21,17 +30,26 @@ int main(int argc, char *argv[])
     server_addr.sin_port = htons(server_port);
     server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-    char *msg = argv[1];
-
     connect(sockid, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-    send(sockid, (const char *)msg, strlen(msg),0);
+    send(sockid, (char *)msg, strlen(msg), 0); 
 
-    int n = recv(sockid, (char *)buffer2, BUFFER_SIZE -1, MSG_WAITALL);
-    buffer2[n] = '\0';
+    recv(sockid, buffer, BUFFER_SIZE, 0); // le hash
+    unsigned char *fileHash = buffer;
+    unsigned char *fileHashHex = binaryToHex(fileHash, strlen(fileHash));
 
-    printf("The message was received from the server: %s", buffer2);
-    puts(buffer);
+    printf("The message was received from the server: %s", fileHashHex);
+    
+    free(buffer);
+    char *buffer = (char*) calloc(BUFFER_SIZE, sizeof(char));
+    recv(sockid, buffer, BUFFER_SIZE, 0); // le fichier
+
+    // // File receive bloc
+
+    // char fileMsg[] = "HASHOK;SENDFILE";
+    // send(sockid, (char *)fileMsg, strlen(fileMsg), 0);
+    // recv(sockid, (char *)buffer2, sizeof(int), MSG_WAITFORONE);
+    // printf("\nLa taille: %d", buffer2);
 
     close(sockid);
 }
--- server/server.c
+++ server/server.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <unistd.h> // for close()
-#include <arpa/inet.h> // for init_addr
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/evp.h>
@@ -18,17 +18,17 @@ void handle_error()
 
 void printHex(unsigned char *buffer, size_t size)
 {
-    for(size_t i = 0; i < size; ++i)
+    for(size_t i=0; i<size; ++i)
         printf("%02x", buffer[i]);
     printf("\n");
 }
 
-void binaryToHex(unsigned char *buffer, size_t size){
+unsigned char* binaryToHex(unsigned char *buffer, size_t size){
     unsigned char *hexString;
     for(size_t i=0; i<size; ++i){
         sprintf(&hexString[i * 2], "%02x", buffer[i]);
     }
-    printf("The hex is: %s", hexString);
+    return hexString;
 }
 
 unsigned int calculateHash(const char* message, unsigned char *hash)
@@ -36,7 +36,8 @@ unsigned int calculateHash(const char* message, unsigned char *hash)
     // OpenSSL_add_all_digests();
     const EVP_MD *md = EVP_get_digestbyname("md5");
     EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-    if (!mdctx)
+
+    if(!mdctx)
     {
         handle_error();
     }
@@ -57,7 +58,7 @@ unsigned int calculateHash(const char* message, unsigned char *hash)
         handle_error();
     }
 
-    printf("\nhashLen: %u\nEVP_MAX_MD_SIZE: %d\n", hashLen, EVP_MAX_MD_SIZE);
+    // printf("\nhashLen: %u\nEVP_MAX_MD_SIZE: %d\n", hashLen, EVP_MAX_MD_SIZE);
     // printHex(hash, hashLen);
     EVP_MD_CTX_free(mdctx);
     return hashLen;
@@ -75,23 +76,22 @@ int main(void)
     server_addr.sin_port = htons(server_port);
     server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-    // char buffer[BUFFER_SIZE];
     char *buffer = (char*) calloc(BUFFER_SIZE, sizeof(char));
 
     int n, len, client_socket;
-
-    int bind_result = bind(sockid, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    int bind_result = bind(sockid, (struct sockaddr*) &server_addr, sizeof(server_addr));
 
     if(bind_result<0)
     {
         printf("Error during binding\n");
+        handle_error();
     }
     else
     {
         printf("Listening on %s:%d\n", server_ip, server_port);
         n = listen(sockid, 1);
 
-        if(n < 0)
+        if(n != 0)
         {
             printf("Error during listen()\n");
             handle_error();
@@ -107,64 +107,75 @@ int main(void)
         }
 
         printf("Accept connection from %s:%d\n",
-                        inet_ntoa(client_addr.sin_addr),
-                        client_addr.sin_port);
+                        inet_ntoa(client_addr.sin_addr), // adresse IP client
+                        client_addr.sin_port); // port
         
-        n = recv(client_socket, buffer, BUFFER_SIZE, MSG_WAITFORONE);
-        buffer[n] = '\0';
+        // recv() ou read() ?
+        n = recv(client_socket, buffer, BUFFER_SIZE, 0); // client envoie un nom de fichier
 
+        // on verifie si le fichier existe
         if(access(buffer, F_OK) != 0)
         {
             printf("This file does not exists\n");
             handle_error();
         }
 
-        printf("\nMessage of size %d received: %s\n",n, buffer);   
+        // printf("\nTaille de message: %d\nMessage: %s\nLongueur de message: %d",
+        //         n, buffer, strlen(buffer));
 
-        const char *fileName = buffer;
+        char *fileName = buffer;
         FILE *file = fopen(fileName, "rb");
 
         if(file == NULL){
             perror("Unable to open file");
             return EXIT_FAILURE;
+            // TODO: utiliser la fonction handle_errors()
         }
 
-        // Pour trouver la taille du fichier en B
+        // Pour trouver la taille du fichier (en Bytes)
         fseek(file, 0, SEEK_END);
         long fileSize = ftell(file);
         fseek(file, 0, SEEK_SET);
 
-        printf("Size of file: %d octets", fileSize);
+        printf("Size of file: %ld bytes", fileSize);
 
-        char fileBuffer[fileSize];        
-        size_t bytesRead = fread(fileBuffer, 1, fileSize, file);
-        if (bytesRead != fileSize) 
-        {
-            perror("Error reading file");
+        // char fileBuffer[fileSize]; // allocation dynamique ou statique ?? Que faire dans le cas de fichiers volumineux ?
+        char *fileBuffer = (char *) malloc(fileSize);
+        if(fileBuffer == NULL){ // Le cas ou fichier est trop grand
             fclose(file);
-            return EXIT_FAILURE;
+            close(sockid);
+            handle_error();
         }
 
-        // Null-terminate the buffer to ensure it is a valid C string
-        fileBuffer[fileSize] = '\0';
+        size_t bytesRead = fread(fileBuffer, 1, fileSize, file);
+        // if(bytesRead != fileSize){
+        //     perror("Error reading file");
+        //     fclose(file);
+        //     fclose(sockid);
+        //     return EXIT_FAILURE;
+        // } // est-ce que ce bloc fait qqch?
 
         // Calculate and print the hash
-
         unsigned char fileHash[EVP_MAX_MD_SIZE];
         unsigned int fileHashSize;
         fileHashSize = calculateHash(fileBuffer, fileHash);
 
-        binaryToHex(fileHash, fileHashSize);
+        unsigned char *fileHashHex = binaryToHex(fileHash, fileHashSize);
+        // printf("\nLe hash: %s", fileHashHex);
+        
+        send(client_socket, fileHash, fileHashSize, 0);
+        printf("\nThis is the message that was sent: ");
+        printHex(fileHash, fileHashSize);
+        printf("\n");
 
+        
+
+        // File send block
         fclose(file);
         free(buffer);
 
-        // char *hello = "Hello from the server";
-        // send(client_socket, fileHash, fileHashSize, 0);
-        // printf("\nThis is the message that was sent: ");
-        // printHex(fileHash, fileHashSize);
+        // recv(client_socket, fileBuffer, fileSize, MSG_WAITALL);
     }
-
     close(client_socket);
     close(sockid);
 }
