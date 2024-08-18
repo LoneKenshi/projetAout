22ddbb0c0d6f069987326a024b07478156e85c40
Sebastian <sebastian0krysiuk@gmail.com>
2024-08-18 11:12:24 -0400

    Client calculates and compares hashes

--- client/client.c
+++ client/client.c
@@ -4,33 +4,67 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <openssl/evp.h>
 
 #define BUFFER_SIZE 1024
 #define HASH_SIZE 16
 #define FILE_NOT_FOUND_MSG "Error: File not found"
 
 unsigned char* binaryToHex(unsigned char *buffer, size_t size) {
-    unsigned char *hexString = (unsigned char*)malloc(size*2+1);
-    for(size_t i = 0; i < size; ++i) {
+    unsigned char *hexString = (unsigned char*)malloc(size * 2 + 1);
+    for (size_t i = 0; i < size; ++i) {
         sprintf(&hexString[i * 2], "%02x", buffer[i]);
     }
-    hexString[size*2] = '\0';
+    hexString[size * 2] = '\0';
     return hexString;
 }
 
+void calculateFileHash(const char *fileName, unsigned char *hash, unsigned int *hashLen) {
+    const EVP_MD *md = EVP_get_digestbyname("md5");
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    FILE *file = fopen(fileName, "rb");
+    
+    if (mdctx == NULL || file == NULL) {
+        perror("Error initializing hash or opening file");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (1 != EVP_DigestInit_ex(mdctx, md, NULL)) {
+        perror("Error initializing digest");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFFER_SIZE];
+    size_t bytes_read;
+
+    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
+        if (1 != EVP_DigestUpdate(mdctx, buffer, bytes_read)) {
+            perror("Error updating digest");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (1 != EVP_DigestFinal(mdctx, hash, hashLen)) {
+        perror("Error finalizing digest");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_MD_CTX_free(mdctx);
+    fclose(file);
+}
+
 int main(int argc, char *argv[]) {
-    if(argc != 4)
-    {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <server_ip> <server_port> <filename>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     char *buffer = (char*) calloc(BUFFER_SIZE, sizeof(char));
     int sockid;
 
-    char *server_ip = argv[1]; // l'ip du serveur
+    char *server_ip = argv[1];
     int server_port = atoi(argv[2]);
-    char *msg = argv[3]; // le nom de fichier
+    char *msg = argv[3];
 
     sockid = socket(AF_INET, SOCK_STREAM, 0);
     if (sockid < 0) {
@@ -50,13 +84,12 @@ int main(int argc, char *argv[]) {
 
     send(sockid, msg, strlen(msg), 0);
 
-    int bytesRead = recv(sockid, buffer, BUFFER_SIZE, 0);
+    int bytesRead = recv(sockid, buffer, HASH_SIZE, 0);
     if (bytesRead <= 0) {
         perror("Error receiving response");
         exit(EXIT_FAILURE);
     }
 
-    // Check if the received message is the file-not-found error
     if (strncmp(buffer, FILE_NOT_FOUND_MSG, strlen(FILE_NOT_FOUND_MSG)) == 0) {
         printf("Server error: %s\n", buffer);
         close(sockid);
@@ -64,31 +97,43 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // Otherwise, assume it's the hash
-    unsigned char *fileHashHex = binaryToHex((unsigned char*)buffer, HASH_SIZE);
-    printf("The hash received from the server: %s\n", fileHashHex);
-    free(fileHashHex);
+    unsigned char receivedHash[HASH_SIZE];
+    memcpy(receivedHash, buffer, HASH_SIZE);
 
     FILE *file = fopen("file2.txt", "wb");
-
-    if (file == NULL)
-    {
+    if (file == NULL) {
         perror("File open error");
         exit(EXIT_FAILURE);
     }
 
-    // Continue to receive the file content
-    while((bytesRead = read(sockid, buffer, BUFFER_SIZE)) > 0) 
-    {
+    while ((bytesRead = read(sockid, buffer, BUFFER_SIZE)) > 0) {
         fwrite(buffer, sizeof(char), bytesRead, file);
     }
 
-    if(bytesRead < 0)
+    if (bytesRead < 0) {
         perror("File receive error");
-    else
+    } else {
         printf("File received successfully\n");
+    }
 
     fclose(file);
+
+    unsigned char calculatedHash[HASH_SIZE];
+    unsigned int hashLen;
+
+    calculateFileHash("file2.txt", calculatedHash, &hashLen);
+
+    if (hashLen != HASH_SIZE) {
+        fprintf(stderr, "Hash length mismatch\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (memcmp(receivedHash, calculatedHash, HASH_SIZE) == 0) {
+        printf("The file hash matches the received hash.\n");
+    } else {
+        printf("The file hash does not match the received hash.\n");
+    }
+
     free(buffer);
     close(sockid);
 
